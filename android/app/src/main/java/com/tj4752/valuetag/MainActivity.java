03e486d2c4e2b3cda64333e91bca2f9cccce828f
Tanuj Singh <tanujsinghkushwah@gmail.com>
2019-08-17 01:14:58 +0530

    Initial commit

---
+++ android/app/src/main/java/com/tj4752/valuetag/MainActivity.java
@@ -0,0 +1,13 @@
+package com.tj4752.valuetag;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
